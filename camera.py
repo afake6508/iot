9e35c280a0203a24ba7ddbf3c1296c0971573db1
afake6508 <146055014+afake6508@users.noreply.github.com>
2023-12-10 15:47:59 +0530

    camera.py

--- camera.py
+++ camera.py
@@ -1,9 +1,31 @@
+'''
+Required Components:
+- Raspberry Pi: 1
+- Power Supply 12 V/2 Amp: 1
+- HDMI Port: 1
+- USB Keyboard: 1
+- USB Mouse: 1
+- Micro SD Card: 1
+- Pi Camera Module: 1
+
+Connection:
+Connect Pi Camera Module to CSI Camera Port
+
+Commands to install Pi-camera packages:
+sudo apt-get install python-picamera
+sudo apt-get install python3-picamera
+sudo pip install picamera
+'''
+
 import time
 import picamera
+
 camera = picamera.PiCamera()
 camera.resolution = (1024, 768)
+
 camera.start_preview()
-time.sleep(2)
-image_path = '/home/pi/Desktop/visitor/images{}.jpg'.format(time.strftime('%Y%m%d%H%M%S'))
-camera.capture(image_path)
+time.sleep(2)  # Wait for 2 seconds to stabilize the camera
+
+camera.capture('/home/pi/Desktop/visitor/images%s.jpg')
+
 camera.stop_preview()
